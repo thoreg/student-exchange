415c3b5c1767114736d29775050ce48453c9f7af
Thomas Rega <thomas.rega@safetyio.com>
2022-02-21 21:35:19 +0100

    Initial commit

---
+++ stud3x/stud3x/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for stud3x project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stud3x.settings')
+
+application = get_wsgi_application()
